2cabe36e0513b3ff12856a5cfe0fcac72208c8c0
AlbertoRD <alberto.rodriguez5493@gmail.com>
2015-09-30 11:00:12 +0200

    Update ejercicios

--- tema3/ej13.php
+++ tema3/ej13.php
@@ -5,22 +5,22 @@ $num1 = $_POST['num1'];
 $num2 = $_POST['num2'];
 $num3 = $_POST['num3'];
 
-if ($num1 <=$num2 and $num1 <=$num3){
-    if($num2<=$num3){
+if ($num1 <= $num2 and $num1 <= $num3) {
+    if ($num2 <= $num3) {
         echo "$num1 < $num2 < $num3";
-    }else{
+    } else {
         echo "$num1 < $num3 < $num2";
     }
-}else if ($num2 <=$num1 and $num2 <=$num3){
-    if($num1<=$num3){
+} else if ($num2 <= $num1 and $num2 <= $num3) {
+    if ($num1 <= $num3) {
         echo "$num2 < $num1 < $num3";
-    }else{
+    } else {
         echo "$num2 < $num3 < $num1";
     }
-}else if ($num3 <=$num1 and $num3 <=$num2){
-    if($num1<=$num2){
+} else if ($num3 <= $num1 and $num3 <= $num2) {
+    if ($num1 <= $num2) {
         echo "$num3 < $num1 < $num2";
-    }else{
+    } else {
         echo "$num3 < $num2 < $num1";
     }
 }
